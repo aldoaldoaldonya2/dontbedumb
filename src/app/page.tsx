17d7bb918f1564bb93f265b5e42ab63947b77cdc
aldoaldoaldonya2 <aldonation77@gmail.com>
2026-03-05 13:59:15 +0700

    feat: marquee section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Hero from "./partials/Hero";
 import About from "./partials/About";
 import Music from "./partials/Music";
 import Story from "./partials/Story";
+import Marquee from "./partials/Marquee";
 import Preloader from "./components/Preloader";
 
 export default function Index() {
@@ -9,6 +10,7 @@ export default function Index() {
     <main className="">
       {/* <Preloader /> */}
       <Hero />
+      <Marquee />
       <About />
       <Music />
       <Story />
