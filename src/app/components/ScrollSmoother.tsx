4501346620b7511a2db1de67d93fe83c455e253a
aldoaldoaldonya2 <aldonation77@gmail.com>
2026-03-08 14:12:13 +0700

    fix: resolve error

--- src/app/components/ScrollSmoother.tsx
+++ src/app/components/ScrollSmoother.tsx
@@ -1,26 +1,30 @@
 'use client';
 
 import { useEffect, useRef } from 'react';
-import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import { ScrollSmoother } from 'gsap/ScrollSmoother';
+import { gsap } from 'gsap/dist/gsap';
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
+import { ScrollSmoother } from 'gsap/dist/ScrollSmoother';
 
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+if (typeof window !== "undefined") {
+  gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+}
 
 const GsapScrollSmoother = ({ children }: { children: React.ReactNode }) => {
   const smoother = useRef<ScrollSmoother | null>(null);
 
   useEffect(() => {
-    smoother.current = ScrollSmoother.create({
-      wrapper: '#smooth-wrapper',
-      content: '#smooth-content',
-      smooth: 1.2,
-      effects: true,
-      smoothTouch: 0.1,
-      normalizeScroll: true,
-    });
+    if (typeof window !== "undefined") {
+      smoother.current = ScrollSmoother.create({
+        wrapper: '#smooth-wrapper',
+        content: '#smooth-content',
+        smooth: 1.2,
+        effects: true,
+        smoothTouch: 0.1,
+        normalizeScroll: true,
+      });
 
-    ScrollTrigger.refresh();
+      ScrollTrigger.refresh();
+    }
 
     return () => {
       if (smoother.current) {
